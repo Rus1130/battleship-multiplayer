49753425cb78d8ca3a6d8c07e9c488bb399feeaa
Rus1130 <arirogozi@gmail.com>
2023-01-25 20:53:06 -0500

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let userArray = [];
+
+io.on('connection', (socket) => {
+    userArray.indexOf("disconnectedUser") === -1 ? userArray.push(socket.id) : userArray[userArray.findIndex(user => user === "disconnectedUser")] = socket.id;
+
+    io.emit('users', userArray);
+
+    socket.on('disconnect', () => {
+        userArray[userArray.findIndex(user => user === socket.id)] = "disconnectedUser";
+
+        io.emit('users', userArray);
+    });
+});
