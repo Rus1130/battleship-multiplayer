c1ae0e7e6033c9a1ea571211e9b856f593f5f6df
Rus1130 <arirogozi@gmail.com>
2023-01-25 22:04:55 -0500

    commit

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const io = new Server(server);
 
-const io = new Server(server, {
-    cors: {
-        origin: "https://Rus1130.github.io'",
-        methods: ["GET", "POST"]
-    }
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
-
-app.set('port', 3000);
-server.listen(3000); 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -21,12 +16,12 @@ app.get('/', (req, res) => {
 let userArray = [];
 
 io.on('connection', (socket) => {
-    userArray.indexOf("disconnectedUser") === -1 ? userArray.push(socket.id) : userArray[userArray.findIndex(user => user === "disconnectedUser")] = socket.id;
+    userArray.indexOf("disconnected") === -1 ? userArray.push(socket.id) : userArray[userArray.findIndex(user => user === "disconnected")] = socket.id;
 
     io.emit('users', userArray);
 
     socket.on('disconnect', () => {
-        userArray[userArray.findIndex(user => user === socket.id)] = "disconnectedUser";
+        userArray[userArray.findIndex(user => user === socket.id)] = "disconnected";
 
         io.emit('users', userArray);
     });
