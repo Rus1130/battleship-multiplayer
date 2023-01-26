05458e50f10c8be98d117428a1b692a1e568fd53
Rus1130 <arirogozi@gmail.com>
2023-01-25 21:24:31 -0500

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: "https://'Rus1130.github.io'",
+        origin: "https://battleship-multiplayer.netlify.app'",
         methods: ["GET", "POST"]
     }
 });
