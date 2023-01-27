55f56d1b89cf8f5eecc8377e747ddb664aac826b
Rus1130 <arirogozi@gmail.com>
2023-01-27 12:50:53 -0500

    commit

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, () => {
+
+http.listen(3000, () => {
     console.log('listening on *:3000');
-  });
+});
+
 
 let messageLog = [];
 let userIDs = [];
