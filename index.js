961751292a8f05b916b7d07d15fe9b17ac03df03
Rus1130 <arirogozi@gmail.com>
2023-01-26 19:20:10 -0500

    commit

--- index.js
+++ index.js
@@ -13,10 +13,34 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+let messageLog = [];
+let userData = [];
+
+
+
 io.on('connection', (socket) => {
+    io.to(socket.id).emit('messageLog', messageLog);
+    console.log(`sent messageLog to client ${socket.id}`)
+
+    
+
+    socket.on('usernameValidation', (data) => {
+        if(userData.includes(data)){
+            io.to(socket.id).emit('usernameValidationResponse', false);
+        } else {
+            io.to(socket.id).emit('usernameValidationResponse', true);
+            userData.push(data);
+        }
+    })
+
     socket.on('clientMessageData', (data) => {
-        console.log('received clientMessageData from client');
+        messageLog.push(data);
+        console.log(`received clientMessageData from client ${socket.id}`);
+
+
         io.emit('newMessageData', data);
         console.log('sent newMessageData to all clients')
+
+
     });
 });
