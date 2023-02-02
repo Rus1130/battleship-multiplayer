387b5f6c088aeaa53d5bf1c4f34245272d1d2d50
Rus1130 <arirogozi@gmail.com>
2023-02-02 14:55:58 -0500

    commit

--- chat-app-encryption.js
+++ chat-app-encryption.js
@@ -1,8 +1,37 @@
-function encrypt(message, recipient){
-    console.log('encrypting message')
+function encrypt(message, recipient, expirationInSecs){
+    let plainMessage = message.split("").map((char) => char.charCodeAt(0))
+    let plainRecipient = recipient.split("").map((char) => char.charCodeAt(0))
+    let plainExpiration = expirationInSecs.toString().split("").map((char) => char.charCodeAt(0))
+
+
+    let shiftProducts = {
+        message: [],
+        recipient: [],
+        expiration: []
+    }
+
+    let shiftRemainders = {
+        message: [],
+        recipient: [],
+        expiration: []
+    }
+
+    /*
+
+        72 >> 5 = 2
+        2 << 5 = 64
+
+        shiftProducts = 2
+        shiftRemainders = 8
+
+
+    */
+
+    console.log(plainMessage, plainRecipient, plainExpiration)
+    return [shiftProducts, shiftRemainders]
 }
 
 
-function decrypt(message, key, recipient){
+function decrypt(encryptedBody, key, currentTimeMs){
     console.log('decrypting message')
 }
