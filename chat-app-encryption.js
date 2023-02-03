27a2a7cc6d08d55a61e67b83467e40a3eacc69f1
Rus1130 <arirogozi@gmail.com>
2023-02-03 13:37:28 -0500

    commit

--- chat-app-encryption.js
+++ chat-app-encryption.js
@@ -1,5 +1,9 @@
 function encrypt(message, recipient, expirationInSecs){
-    expirationInSecs = Date.now() + (expirationInSecs * 1000)
+    if(expirationInSecs != 0){
+        expirationInSecs = Date.now() + (expirationInSecs * 1000)
+    } else {
+        expirationInSecs = 0
+    }
     let plainMessage = message.split("").map((char) => char.charCodeAt(0))
     let plainRecipient = recipient.split("").map((char) => char.charCodeAt(0))
     let plainExpiration = expirationInSecs.toString().split("").map((char) => char.charCodeAt(0))
@@ -92,17 +96,12 @@ function encrypt(message, recipient, expirationInSecs){
     decode:
     ((shiftProduct << 5) - randomAddition) + shiftRemainder
 
-
     */
-
-    console.log(shiftProducts, shiftRemainders, randomAdditions)
-    console.log(plainMessage, plainRecipient, plainExpiration)
     return result;
 }
 
 
 function decrypt(body, sender){
-    let currentTime = Date.now()
     let [lengths, encryptedMessage] = body.split(":")
 
     let sectorLength = lengths.split("-").map(num => parseInt(num, 36))
@@ -111,37 +110,68 @@ function decrypt(body, sender){
 
     lengths = lengths.split("-").map(num => parseInt(num, 36))
 
-    let shiftProducts = {
-        message: [],
-        recipient: [],
-        expiration: []
-    }
+    let shiftProducts = []
 
-    let shiftRemainders = {
-        message: [],
-        recipient: [],
-        expiration: []
-    }
+    let shiftRemainders = []
 
-    let randomAdditions = {
-        message: [],
-        recipient: [],
-        expiration: []
-    }
+    let randomAdditions = []
     
     // for each index in sectors, insert a character at the lengths[0], lengths[1], and lengths[2] indexes
     
     for(i = 0; i < sectors.length; i++){
         sectors[i] = sectors[i].match(new RegExp(`.{1,${lengths[0]}}`, "g"))
+        sectors[i][1] = sectors[i][1].concat(sectors[i][2])
+        sectors[i].splice(2, 1)
+        // place a '-' at index lengths[1] in sectors[0][1]
+        sectors[i][1] = sectors[i][1].slice(0, lengths[1]) + "-" + sectors[i][1].slice(lengths[1])
+        sectors[i][1] = sectors[i][1].split("-")
+        sectors[i].push(sectors[i][1][1])
+        sectors[i][1] = sectors[i][1][0]
     }
 
-    sectors[0][1] = sectors[0][1].concat(sectors[0][2])
-    sectors[0].splice(2, 1)
+    shiftProducts.push(sectors[0][0])
+    shiftProducts.push(sectors[0][1])
+    shiftProducts.push(sectors[0][2])
+
+    shiftRemainders.push(sectors[1][0])
+    shiftRemainders.push(sectors[1][1])
+    shiftRemainders.push(sectors[1][2])
+
+    randomAdditions.push(sectors[2][0])
+    randomAdditions.push(sectors[2][1])
+    randomAdditions.push(sectors[2][2])
+
+    shiftProducts = shiftProducts.map(num => num.split("").map(x => +x))
+    shiftRemainders = shiftRemainders.map(num => num.split("").map(x => parseInt(x, 36)))
+    randomAdditions = randomAdditions.map(num => num.split("").map(x => x.charCodeAt(0)))
 
 
+    let message = []
+    let recipient = []
+    let expiration = []
 
-    
-    console.log(lengths)
-    console.log(sectors)
-    return {shiftProducts: shiftProducts, shiftRemainders: shiftRemainders, randomAdditions: randomAdditions}
+
+    for(j = 0; j < shiftProducts[0].length; j++){
+        message.push(((shiftProducts[0][j] << 5) - randomAdditions[0][j]) + shiftRemainders[0][j])
+    }
+
+    for(j = 0; j < shiftProducts[1].length; j++){
+        recipient.push(((shiftProducts[1][j] << 5) - randomAdditions[1][j]) + shiftRemainders[1][j])
+    }
+
+    for(j = 0; j < shiftProducts[2].length; j++){
+        expiration.push(((shiftProducts[2][j] << 5) - randomAdditions[2][j]) + shiftRemainders[2][j])
+    }
+
+    message = message.map(num => String.fromCharCode(num)).join("")
+    recipient = recipient.map(num => String.fromCharCode(num)).join("")
+    expiration = +(expiration.map(num => +String.fromCharCode(num)).join(""))
+
+    if(expiration < Date.now() && expiration != 0){
+        return 'Message expired!'
+    } else if(recipient != sender && recipient != 'all'){
+        return 'invalid recipient!'
+    } else {
+        return message
+    }
 }
